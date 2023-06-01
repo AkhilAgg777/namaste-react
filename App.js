109622c5f42de95fda5b14255e2b164dd423a5de
Akhil Aggarwal <akhilaggarwal100@gmail.com>
2023-06-01 20:41:07 +0530

    Episode-03 V1

--- App.js
+++ App.js
@@ -1,18 +1,10 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
 
-const parent = React.createElement("div", { id: "parent" }, [
-  React.createElement("div", { id: "child" }, [
-    React.createElement("h1", {}, "I'm an h1 tag"),
-    React.createElement("h2", {}, "I'm an h2 tag"),
-  ]),
-  React.createElement("div", { id: "child2" }, [
-    React.createElement("h1", {}, "I'm an h1 tag"),
-    React.createElement("h2", {}, "I'm an h2 tag"),
-  ]),
-]);
+// React.createElement =>Object => HTMLElement(render)
 
-console.log(parent); //object
-const root = ReactDOM.createRoot(document.getElementById("root"));
+const heading=React.createElement("h1",{id:"heading"},"Namaste React");
 
-root.render(parent);
+const root=ReactDOM.createRoot(document.getElementById("root"));
+
+root.render(heading);
