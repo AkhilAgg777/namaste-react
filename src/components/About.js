2c58b15640f136c0731fbc8333d3236e03f7f796
Akhil Aggarwal <akhilaggarwal100@gmail.com>
2024-01-08 21:03:35 +0530

    Episode-8  Let's get Classy

--- src/components/About.js
+++ src/components/About.js
@@ -1,10 +1,38 @@
-const About = () => {
-  return (
-    <div>
-      <h1>About</h1>
-      <h2>This is Akhil</h2>
-    </div>
-  );
-};
+import User from "./User";
+import UserClass from "./UserClass";
+import { Component } from "react";
+
+class About extends Component {
+  constructor(props) {
+    super(props);
+    //console.log("Parent Constructor");
+  }
+
+  componentDidMount(){
+    //console.log("Parent Component Did Mount");
+  }
+  render() {
+    //console.log("Parent Render");
+    return (
+      <div>
+        <h1>About Class Component</h1>
+        <h2>This is Akhil</h2>
+        {/* <User name={"Akhil Aggarwal(function)"}/> */}
+        <UserClass name={"First"} location={"Dehradun class"} />
+      </div>
+    );
+  }
+}
+
+// const About = () => {
+//   return (
+//     <div>
+//       <h1>About</h1>
+//       <h2>This is Akhil</h2>
+//       {/* <User name={"Akhil Aggarwal(function)"}/> */}
+//       <UserClass name={"Akhil Aggarwal(class)"} location={"Dehradun class"}/>
+//     </div>
+//   );
+// };
 
 export default About;
